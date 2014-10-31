9d1364cfcd1d200ab0613e35e1d52b5d8bb8425d
KERYELL-EVEN Elouan <ekeryelleven@chauvet.pedago.ipb.fr>
2014-10-31 17:30:39 +0100

    interface file added

---
+++ TDP2/modele.h
@@ -0,0 +1,14 @@
+#ifndef __MODELE_H__
+#define __MODELE_H_
+
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct _Particule {
+  double mass;
+  double x;
+  double y;
+  double v;
+} Particule;
+
+#endif
