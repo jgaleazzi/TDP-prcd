4a92c604c415166721477205a5f4a01cd6380979
GALEAZZI Jean-francois <jgaleazzi@barabahau.pedago.ipb.fr>
2014-10-31 17:00:17 +0100

    fichier de génération des particules

---
+++ gen_part.py
@@ -0,0 +1,29 @@
+import numpy as np
+import sys
+
+def gen():
+    if (len(sys.argv) <3):
+        print("Please enter a number of proc and number of particle")
+    else:
+        nb_proc = int(sys.argv[1])
+        nb_part = int(sys.argv[2])
+
+
+    Masse_tab = (10000*np.random.rand(nb_part)).tolist()
+    x_tab =  (100*np.random.rand(nb_part)).tolist()
+    y_tab =  (100*np.random.rand(nb_part)).tolist()
+    vit_tab = (100*np.random.rand(nb_part)).tolist()
+    nb_element_file = nb_part/nb_proc
+    reste = nb_part%nb_proc
+    count = 0
+    for i in range(nb_proc):
+        NomFichier = 'proc_'+str(i)+'.txt'
+        Fichier = open(NomFichier,'w')
+        for j in range(nb_element_file):
+            Fichier.write(str(count)+ " " + str(Masse_tab[j])+ " " + str(x_tab[j])+ " " + str(y_tab[j])+ " " + str(vit_tab[j])+ '\n')
+            count+=1
+        for k in range(reste):
+            Fichier.write(str(ID_tab[count])+ " " + str(Masse_tab[k])+ " " + str(x_tab[k])+ " " + str(y_tab[k])+ " " + str(vit_tab[k])+ '\n')
+            count+=1
+        Fichier.close()
+gen()
