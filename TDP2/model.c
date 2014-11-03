d1e605fcb384a00e4be983827742d5e5d70243af
jgaleazzi <jgaleazzi@enseirb-matmeca.fr>
2014-11-03 14:21:18 +0100

    ajout nouvelle struct et fonctions

--- TDP2/model.c
+++ TDP2/model.c
@@ -1,8 +1,41 @@
-double compute_distance(Particle *partA, Particle *partB){
-  double dx = (partB.x-partA.x)*(partB.x-partA.x);
-  double dy = (partB.y-partA.y)*(partB.y-partA.y);
+#include "model.h"
+
+double compute_norm(Particle *partA, Particle *partB){
+  double dx = (partB->x-partA->x)*(partB->x-partA->x);
+  double dy = (partB->y-partA->y)*(partB->y-partA->y);
   double dist = sqrt(dx + dy);
   return dist;
 }
 
+void compute_gravitation(Particle *partA, Particle *partB, Movement *moveA){
+  double norm =  compute_norm(partA,partB);
+  double force = G* partB->mass/(norm*norm);
+  //decomposition on x and y (unit vector)
+  double vect_x = (partB->x - partA->x)/norm;
+  double vect_y=(partB->y - partA->y)/norm;
 
+  moveA->a_x += force*vect_x;
+  moveA->a_y += force*vect_y;
+}
+
+void gravitation(Particle *parts, Movement *moves, int nb_part){
+  int i,j;
+  //describe all the parts
+  for(i=0;i<nb_part;i++){
+    for(j=0;j<nb_part;i++){
+      if(i!=j)
+	compute_gravitation(&parts[i],&parts[j],&moves[i]);
+    }
+  }
+}
+
+
+void update_moves(Particle *parts, Movement *moves, int nb_part){
+  int i;
+  for(i=0;i<nb_part;i++){
+    moves[i]->v_x += moves[i]->a_x*dt;
+    moves[i]->v_y += moves[i]->a_y*dt;
+    parts[i]->x+= moves[i]->v_x*dt +  moves[i]->a_x*(dt*dt)/2;
+    parts[i]->y+= moves[i]->v_y*dt +  moves[i]->a_y*(dt*dt)/2;
+  }
+}
--- TDP2/model.h
+++ TDP2/model.h
@@ -5,17 +5,38 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
+#include <math.h>x
 
 #define CHK(X, Y) do { if ((X)<0) {perror(Y); exit(1);} } while(0)
+#define G 1.0 //gravitation coefficient
+
+
 
 typedef struct _Particle {
   double mass;
   double x;
   double y;
-  double v;
 } Particle;
+
+typedef struct _Movement{
+  double v_x;
+  double v_y;
+  double a_x;
+  double a_y;
+}Movement;
 
 //computes the new dt by solving a degree-2 polynom
 double compute_dt(const double acceleration_norm, const double speed_norm, double minimal_distance);
 
+//computes distance between 2 part
+double compute_norm(Particle *partA, Particle *partB);
+
+//compute the gravitation force between two parts
+void compute_gravitation(Particle *partA, Particle *partB, Movement *moveA);
+
+//compute the gravitation force for each part
+void gravitation(Particle *parts, Movement *moves, int nb_part);
+
+//update of the speed and positions
+void update_moves(Particle *parts, Movement *moves, int nb_part);
 #endif
