670502cbee26ea78a31cc8528de9def1a3df0dd9
jgaleazzi <jgaleazzi@enseirb-matmeca.fr>
2014-11-03 15:10:30 +0100

    javais alloué deux fois particle

--- TDP2/main.c
+++ TDP2/main.c
@@ -9,18 +9,18 @@ int main(int argc, char * argv[]){
   int err;
   sprintf(name,"proc_%d.txt",num_proc);   
   printf("%s\n",name);     
-  int nb_line = nb_lines(name); 
-  particle = malloc(nb_line*sizeof(Particle));
+  int nb_part = nb_lines(name); 
+
   FILE* file = fopen(name, "r");                                                     
   if(!file){                                                                         
     fprintf(stderr, "Cannot open file %s\n", name);                                  
   }
-  particle = malloc(nb_line*sizeof(Particle));
-  move=malloc(nb_line*sizeof(Movement));
-  load(particle,move,file,nb_line);
+  particle = malloc(nb_part*sizeof(Particle));
+  move=malloc(nb_part*sizeof(Movement));
+  load(particle,move,file,nb_part);
   fclose(file);
 
-  printf("part :%lf %lf %lf \n",particle[0].mass,particle[0].x,particle[0].y);
+  printf("part :%lf %lf %lf %lf\n",particle[0].mass,particle[0].x,particle[0].y,particle[0].dist);
   printf("move: %lf %lf %lf %lf\n",move[0].v_x,move[0].v_y,move[0].a_x,move[0].a_y);
 
   free(particle);
