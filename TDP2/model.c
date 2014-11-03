f00e9f02bef91433874d592928effbc50d7b9c60
jgaleazzi <jgaleazzi@enseirb-matmeca.fr>
2014-11-03 15:17:56 +0100

    ajout fonction et corrrection bug

--- TDP2/model.c
+++ TDP2/model.c
@@ -9,6 +9,9 @@ double compute_norm(Particle *partA, Particle *partB){
 
 void compute_gravitation(Particle *partA, Particle *partB, Movement *moveA){
   double norm =  compute_norm(partA,partB);
+  if (partA->dist == 0.0)
+    partA->dist = norm;
+  partA->dist = min(partA->dist,norm);
   double force = G* partB->mass/(norm*norm);
   //decomposition on x and y (unit vector)
   double vect_x = (partB->x - partA->x)/norm;
@@ -18,24 +21,50 @@ void compute_gravitation(Particle *partA, Particle *partB, Movement *moveA){
   moveA->a_y += force*vect_y;
 }
 
-void gravitation(Particle *parts, Movement *moves, int nb_part){
+void gravitation(Particle *parts, Movement *moves){
   int i,j;
   //describe all the parts
   for(i=0;i<nb_part;i++){
     for(j=0;j<nb_part;i++){
       if(i!=j)
-	compute_gravitation(&parts[i],&parts[j],&moves[i]);
+       	compute_gravitation(&parts[i],&parts[j],&moves[i]);
     }
   }
 }
 
 
-void update_moves(Particle *parts, Movement *moves, int nb_part){
+void update_moves(Particle *parts, Movement *moves){
   int i;
   for(i=0;i<nb_part;i++){
-    moves[i]->v_x += moves[i]->a_x*dt;
-    moves[i]->v_y += moves[i]->a_y*dt;
-    parts[i]->x+= moves[i]->v_x*dt +  moves[i]->a_x*(dt*dt)/2;
-    parts[i]->y+= moves[i]->v_y*dt +  moves[i]->a_y*(dt*dt)/2;
+    moves[i].v_x += moves[i].a_x*dt;
+    moves[i].v_y += moves[i].a_y*dt;
+    parts[i].x+= moves[i].v_x*dt +  moves[i].a_x*(dt*dt)/2;
+    parts[i].y+= moves[i].v_y*dt +  moves[i].a_y*(dt*dt)/2;
   }
+}
+
+void update_dt(Particle *parts, Movement *moves){
+  /* Solve for each part
+     AX²/2+BX-0.1*C=0
+   where A = ||acc_part ||
+             B = || vit_part ||
+	     C = part_distmin
+  */
+
+  double A, B, C,delta;
+  int i;
+  for(i=0;i<nb_part;i++){
+    A = sqrt(moves[i].a_x*moves[i].a_x + moves[i].a_y*moves[i].a_y); 
+    B = sqrt(moves[i].v_x*moves[i].v_x + moves[i].v_y*moves[i].v_y); 
+    C = parts[i].dist;
+    
+    delta = B*B + 0.2 * A*C;
+
+  }
+}
+
+double min(double a, double b){
+  if (a<b)
+    return a;
+  return b;
 }
