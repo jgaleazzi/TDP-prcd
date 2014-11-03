c04daee3f728a2214feaf5211f2c2f886764672a
jgaleazzi <jgaleazzi@enseirb-matmeca.fr>
2014-11-03 14:20:38 +0100

    separation pour remplir les dux structures

--- TDP2/load.c
+++ TDP2/load.c
@@ -16,10 +16,12 @@ int nb_lines (char *name){
 }
 
 
-void load(Particle *particle,FILE* file, int nb_line){
+void load(Particle *particle,Movement* move, FILE* file, int nb_line){
   int i,err;
   for(i=0;i<nb_line;i++){
-    CHK(err=  fscanf(file, "%lf %lf %lf %lf",&particle[i].mass,&particle[i].x,&particle[i].y,&particle[i].v),"scanf");
+    CHK(err=  fscanf(file, "%lf %lf %lf %lf %lf",&particle[i].mass,&particle[i].x,&particle[i].y,&move[i].v_x,&move[i].v_y),"scanf");
+    move[i].a_x=0.0;
+    move[i].a_y=0.0;
   
   }
 }
