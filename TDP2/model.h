5bd3d36bb1caca0f2a300e168a1a4604ec005a5f
jgaleazzi <jgaleazzi@enseirb-matmeca.fr>
2014-11-03 15:10:53 +0100

    ajout fonction update_dt

--- TDP2/model.h
+++ TDP2/model.h
@@ -5,17 +5,19 @@
 #include <stdlib.h>
 #include <string.h>
 #include <assert.h>
-#include <math.h>x
+#include <math.h>
 
 #define CHK(X, Y) do { if ((X)<0) {perror(Y); exit(1);} } while(0)
 #define G 1.0 //gravitation coefficient
+#define DT_max
 
-
-
+double dt;
+int nb_part;
 typedef struct _Particle {
   double mass;
   double x;
   double y;
+  double dist;
 } Particle;
 
 typedef struct _Movement{
@@ -35,8 +37,13 @@ double compute_norm(Particle *partA, Particle *partB);
 void compute_gravitation(Particle *partA, Particle *partB, Movement *moveA);
 
 //compute the gravitation force for each part
-void gravitation(Particle *parts, Movement *moves, int nb_part);
+void gravitation(Particle *parts, Movement *moves);
 
 //update of the speed and positions
-void update_moves(Particle *parts, Movement *moves, int nb_part);
+void update_moves(Particle *parts, Movement *moves);
+
+//update dt
+void update_dt(Particle *parts, Movement *moves);
+
+double min(double a, double b);
 #endif
