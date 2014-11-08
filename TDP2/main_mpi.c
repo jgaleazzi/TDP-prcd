15529e0a9e0c7fa8f10dc78012f2fc41690b7a22
jgaleazzi <jgaleazzi@enseirb-matmeca.fr>
2014-11-08 16:54:57 +0100

    a tester avec le schema

--- TDP2/main_mpi.c
+++ TDP2/main_mpi.c
@@ -1,12 +1,14 @@
+
 #include "load.h"
-#include "mpi.h"
+#include <mpi.h>
 
 int main(int argc, char * argv[]){
-  Particle * particles;
+  Particle * particles_owned, *buffer_1, *buffer_2;
   Movement *moves;
   int myrank, comm_size;
+  MPI_Status status;
 
-  MPI_INIT(NULL, NULL);
+  MPI_Init(NULL, NULL);
   MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
   MPI_Comm_size(MPI_COMM_WORLD, &comm_size);
 
@@ -26,58 +28,110 @@ int main(int argc, char * argv[]){
   if(!file){
     fprintf(stderr, "Cannot open file %s\n", name);
   }
-  particles = malloc(nb_part*sizeof(Particle));
-  moves=malloc(nb_part*sizeof(Movement));
-  load(particles,moves,file,nb_part);
+  particles_owned = malloc(nb_part*sizeof(Particle));
+  buffer_1 = malloc(nb_part*sizeof(Particle));
+  buffer_2 = malloc(nb_part*sizeof(Particle));
+  moves = malloc(nb_part*sizeof(Movement));
+  load(particles_owned,moves,file,nb_part);
   fclose(file);
 
   //DEBUG                                                                                                                                                                        
-  //printf("part :%lf %lf %lf %lf\n",particles[0].mass,particles[0].x,particles[0].y,particles[0].dist);                                                                         
+  //  printf("part :%lf %lf %lf %lf\n",particles_owned[0].mass,particles_owned[0].x,particles_owned[0].y,particles_owned[0].dist);                                                                         
   //printf("move: %lf %lf %lf %lf\n",moves[0].v_x,moves[0].v_y,moves[0].a_x,moves[0].a_y);                                                                                       
   //DEBUG                                                                                                                                                                        
 
-  int buf_size = NB_PARTS%comm_size ? NB_PARTS/comm_size + 1 : NB_PARTS/comm_size;
-  int buffer0[buf_size];
-  int buffer1[buf_size];
-  int i;
-  int pic_counter = 1;
+  int array_of_lengths[] = { 1, 1, 1, 1};
+  MPI_Aint array_of_displs[4];
+  MPI_Aint i1,i2;
+
+  MPI_Get_address(&particles_owned[0],&i1);
+  MPI_Get_address(&particles_owned[0].mass,&i2);
+  array_of_displs[0]= i2 - i1;
+  MPI_Get_address(&particles_owned[0].x,&i2);
+  array_of_displs[1]= i2 - i1;
+  MPI_Get_address(&particles_owned[0].y,&i2);
+  array_of_displs[2]= i2 - i1;
+  MPI_Get_address(&particles_owned[0].dist,&i2);
+  array_of_displs[3]= i2 - i1;
+
+  MPI_Datatype array_of_types[] = { MPI_DOUBLE, MPI_DOUBLE,MPI_DOUBLE,MPI_DOUBLE};
+  MPI_Datatype A_PARTICLE;
+  MPI_Type_struct( 4,array_of_lengths,array_of_displs,array_of_types,&A_PARTICLE);
+  MPI_Type_commit(&A_PARTICLE);
+
+  MPI_Datatype PARTICLES;
+  MPI_Type_vector(nb_part, 1, 1, A_PARTICLE, &PARTICLES);
+  MPI_Type_commit(&PARTICLES);
+  
+
+  /*initialisation persistant communication*/
+  MPI_Request buffer_send1, buffer_send2;
+  MPI_Request buffer_recv1, buffer_recv2;
+  MPI_Send_init(buffer_1, 1 ,PARTICLES , (myrank+1)%comm_size, 99, MPI_COMM_WORLD, &buffer_send1 );
+  MPI_Recv_init(buffer_2, 1, PARTICLES,(myrank-1)%comm_size, 99, MPI_COMM_WORLD, &buffer_recv1);
+  MPI_Send_init(buffer_2, 1 ,PARTICLES , (myrank+1)%comm_size, 99, MPI_COMM_WORLD, &buffer_send2 );
+  MPI_Recv_init(buffer_1, 1, PARTICLES,(myrank-1)%comm_size, 99, MPI_COMM_WORLD, &buffer_recv2);
+
+  int i,ring_index;
   for(i=0;i<NB_ITER;i++){
+    MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
 
-   int* send_p;
-    int* rcv;
-    int j = 0;
-
-    //1st step : each proc computes on its own particles                                                                                                                         
-
-    for(j = 0; j < comm_size; ++j)
-      {
-        if(j == 0)
-          {
-
-          }
-
-      }
-
-    gravitation(particles,moves);
-    update_moves(particles, moves);
-    update_dt(particles, moves);
-
-    if(!(i%PIC_FREQ))
-      {
-        char output[10];
-        sprintf(output, "%d_out%d.txt", myrank, pic_counter);
-        FILE *file = fopen(output, "w+");
-        int j;
-        for(j=0; j<nb_part; j++){
-          fprintf(file, "%lf %lf\n", particles[j].x, particles[j].y);
+    printf("myrank debut de boulce %d\n",myrank);    
+    memcpy(buffer_1, particles_owned, nb_part*sizeof(Particle));
+    for(ring_index;ring_index<comm_size; ring_index++){
+      /* iteration number in ring  even*/
+      if(ring_index%2==0){
+	MPI_Start(&buffer_send1);
+	MPI_Start(&buffer_recv1);
+	if(ring_index==0){
+	  gravitation_inter(particles_owned,moves);
 	}
-        fclose(file);
-	++pic_counter;
+	else{
+	  gravitation(particles_owned,buffer_1,moves);
+	}
+	MPI_Wait(&buffer_send1, &status);
+	MPI_Wait(&buffer_recv1, &status);
       }
-  }
-  free(particles);
-  free(moves);
+      /* iteration number in ring odd*/
+      else{
+	MPI_Start(&buffer_send2);
+	MPI_Start(&buffer_recv2);
+	gravitation(particles_owned,buffer_1,moves);
+	
+	MPI_Wait(&buffer_send2, &status);
+	MPI_Wait(&buffer_recv2, &status);
+      
+      }    
+    }
+    update_moves(particles_owned, moves);
 
+  
+    char output[10];
+    printf("myrank avant ecriture de boulce %d\n",myrank);
+
+    sprintf(output, "test/%d_out%d.txt", myrank, i+1);
+    FILE *file = fopen(output, "w+");
+    int j;
+    for(j=0; j<nb_part; j++){
+      fprintf(file, "%lf %lf\n", particles_owned[j].x, particles_owned[j].y);
+
+    }
+    double my_dt = update_dt(particles_owned, moves);
+    MPI_Allreduce(&my_dt, &dt, 1, MPI_DOUBLE, MPI_MIN, MPI_COMM_WORLD);
+
+    fclose(file);
+    printf("myrank fin de boulce %d\n",myrank);
+  }
+ 
+  MPI_Type_free(&A_PARTICLE);
+  MPI_Type_free(&PARTICLES);
   MPI_Finalize();
+ 
+
+  free(particles_owned);
+ free(moves);
+  free(buffer_1);
+  free(buffer_2);
   return 0;
 }
+  
