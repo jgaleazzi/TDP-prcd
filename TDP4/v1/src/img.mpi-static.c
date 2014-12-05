38ed5ef6f4359818b75643028433d104cfe0b1cc
n0n0 <arnodurocher@gmail.com>
2014-12-05 16:23:26 +0100

    debut mpi

--- TDP4/v1/src/img.mpi-static.c
+++ TDP4/v1/src/img.mpi-static.c
@@ -26,6 +26,9 @@
 #include "bnd.h"
 #include "cmr.h"
 
+#define BLOCK_SIZE 8
+
+static int myrank, nb_proc;
 
 typedef struct {
   COUPLE  Pixel;
@@ -62,6 +65,46 @@ pixel_basic (INDEX i, INDEX j)
   return (Ray.Color);
 }
 
+/**
+/* * Create the mpi datatype used to describe the blocks in the matrix
+ * @param MPI_Type_block is the datatype to create (output)
+ * @param block_size a block contains block_size^2 elements
+ * @param mat_size a block contains mat_size^2 elements
+ **/
+static void create_datatype_bloc(MPI_Datatype* MPI_Type_block, int block_size, int mat_size)
+{
+  MPI_Type
+  MPI_Type_vector(block_size, block_size, mat_size, MPI_Type_Color, MPI_Type_block);
+  MPI_Type_create_resized(*MPI_Type_block, 0, block_size*sizeof(double), MPI_Type_block);
+  MPI_Type_commit(MPI_Type_block);
+}
+
+struct matrix matrix_gather(int N, double* block, int root, MPI_Comm comm)
+{
+  if(myrank == root)
+  {
+    //Allocate memory for the gathered matrix
+    res = //TODO
+
+    MPI_Datatype MPI_Type_block;
+    create_datatype_bloc(&MPI_Type_block,BLOCK_SIZE,res.length);
+    int recvcounts[nb_proc];
+    int displs[nb_proc];
+    int i;
+    for(i=0; i<nb_proc; i++)
+    {
+      recvcounts[i] = 1;
+      displs[i] = i%grid->N + (i/grid->N)*grid->N*n;
+    }
+    MPI_Gatherv(block->tab, n*n, MPI_DOUBLE, res.tab, recvcounts, displs, MPI_Type_block, root, comm);
+  }
+  else
+  {
+    MPI_Gatherv(block->tab, n*n, MPI_DOUBLE, NULL, NULL, NULL, NULL, root, grid->comm_grid);
+  }
+
+  return res;
+}
 
 void
 img (const char *FileNameImg)
@@ -78,6 +121,11 @@ img (const char *FileNameImg)
   fprintf (FileImg, "P6\n%d %d\n255\n", Img.Pixel.i, Img.Pixel.j);
   INIT_MEM (TabColor, Img.Pixel.i, COLOR);
 
+  MPI_Init(NULL,NULL);
+  
+  MPI_Comm_rank(MPI_COMM_WORLD, &myrank);
+  MPI_Comm_size(MPI_COMM8WORLD, &nb_proc);
+
   for (j = 0; j < Img.Pixel.j; j++) {
     for (i = 0; i < Img.Pixel.i; i++) {
       TabColor [i] = pixel_basic (i, j);
@@ -92,6 +140,8 @@ img (const char *FileNameImg)
     }
     fflush (FileImg);
   }
+
+  MPI_Finalize();
 
   EXIT_MEM (TabColor);
   EXIT_FILE (FileImg);
